145b67f9449af788970618804d9853c699cad7f4
Ilya Dolmatov <dolmatoffilya@gmail.com>
2017-11-16 15:12:51 +0200

    work with dropzone

--- routes/upload.js
+++ routes/upload.js
@@ -8,48 +8,22 @@ exports.post = function (req, res) {
         return res.status(400).send('No files were uploaded.');
     }
 
-    var arr;
-    if (Array.isArray(req.files.files)) {
-        arr = req.files.files;
-    } else {
-        arr = [req.files.files];
-    }
+    var sampleFile = req.files.file;
+    var path = '/images/' + sampleFile.name;
+    sampleFile.mv('./public' + path, function (err) {
+        if (err) {
+            console.log(err);
+            return res.status(500).send(err);
+        }
 
-    var titles = req.body.title.split(',');
-    var descs = req.body.desc.split(',');
-    var years = req.body.year.split(',');
-    var credits = req.body.credits.split(',');
+        var img = new Image(images.id, path, '', '', '', '');
+        images.images.imgs.push(img.toJSON());
+        images.id++;
 
-    var json = [];
-    var m = arr.length;
-
-    arr.forEach(function (item, i) {
-        var sampleFile = item;
-        var path = '/images/' + sampleFile.name;
-        sampleFile.mv('./public' + path, function (err) {
-            if (err) {
-                console.log(err);
-                return res.status(500).send(err);
-            }
-
-            var title = titles[i];
-            var desc = descs[i];
-            var year = years[i];
-            var credit = credits[i];
-
-            var img = new Image(images.id, path, title, desc, credit, year);
-            images.images.imgs.push(img.toJSON());
-            images.id++;
-            json.push(img.toJSON());
-
-            if (--m === 0) {
-                console.log(json);
-                res.json({
-                    success_: true,
-                    error_: 0,
-                    text_: "Done!",
-                    toAdd: json});
-            }
-        });
+        res.json({
+            success_: true,
+            error_: 0,
+            text_: "Done!",
+            toAdd: img});
     });
 };
